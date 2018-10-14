8a3905cc1ee1ce791b9d17e12d7b8549473ee9d4
gzsoft <gzsoft@f90a7c9b-23a7-4442-b2e3-48d2b53e0687>
2018-10-14 22:48:33 +0000

    +adding src

---
+++ MApp/scripts/login.js
@@ -0,0 +1,27 @@
+﻿/*
+This file in the main entry point for defining grunt tasks and using grunt plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
+*/
+$(document).ready(function () {
+    $('.tt').tooltip();
+
+    $('#but').click(function () {
+        swal({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.value) {
+                swal(
+                  'Deleted!',
+                  'Your file has been deleted.',
+                  'success'
+                )
+            }
+        })
+    });
+});
